f16faf011617f2afa6178d29bb474bf85c437bf1
Hannah Lodge <lodg0021@umn.edu>
2022-03-26 20:00:47 -0500

    working on timer function

---
+++ assets/script.js
@@ -0,0 +1,76 @@
+// console.log("it'sworking");
+// link to question
+var question_box = document.querySelector("questionspace");
+
+// carousel buttons
+var start = document.getElementById("startBtn");
+
+//index variables and fillers
+var index=0;
+var nowQuestion;
+
+// question array
+var questionText=[
+    {"question":"Question1?",
+        "choices": ["A", "B", "C", "D"],
+        "answer": "B",
+    },
+
+    {"question":"Question2?",
+        "choices": ["A", "B", "C", "D"],
+        "answer": "B",
+    }
+];
+
+// create timer on start of start button
+function quizTimer() {
+    setInterval(function() {
+        timeSec--;
+        if (timeSec>0){
+            var displayTime= document.getElementById("timer");
+            displayTime.textContent = timeSec;
+        }
+        else {
+            clearInterval(quizTimer);
+            alert("Time is up.");
+        }
+
+        displayQ()
+
+    },1000);
+}
+
+var timeSec= 500;
+
+function displayQ(){
+console.log ("its working")
+};
+
+//function to check if answer correct on selecting submit button
+function correctAnswer () {
+//   console.log('its working');
+
+
+}
+// navigation function
+function nextQuestion() {
+
+}
+
+// listener for the buttons
+start.addEventListener("onclick", ()=>quizTimer);
+
+// start.addEventListener("click", console.log("its working"));
+
+
+//function for display right or wrong after answer questions
+
+function rightWrong(){
+
+};
+
+//score sheet at the end
+
+function score(){
+
+};
